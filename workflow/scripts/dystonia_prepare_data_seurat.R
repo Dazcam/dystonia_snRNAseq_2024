3d91c5ecb7ea816bae15b0e341fc66741f4488ce
Dazcam <camerond@cardiff.ac.uk>
2024-03-07 15:49:59 +0000

    Alter scripts to handle local and remote use

--- workflow/scripts/dystonia_prepare_data_seurat.R
+++ workflow/scripts/dystonia_prepare_data_seurat.R
@@ -11,65 +11,18 @@
 #  Using Seurat 5 primarily, but also bioconductor packages for QC 
 #  Some features not running locally due to limited resources: need to migrate to Hawk
 
-##  Load Packages  --------------------------------------------------------------------
-library(BPCells)
-library(Seurat)
-library(SeuratObject)
-library(SeuratDisk)
-library(SeuratWrappers)
-library(Azimuth) 
-library(tidyverse)
-library(scCustomize)
-library(readxl)
-library(cowplot)
-library(scuttle)
-library(scater)
+##  Set local to local or remote -----------------------------------------------------
+# 
+locale <- 'local'
+if (locale == 'local') { 
+  root_dir <- '~/Desktop/dystonia_snRNAseq_2024/'
+  region <- 'str'}
+if (locale == 'remote') { 
+  root_dir <- snakemake@params[['root_dir']]
+  region <- snakemake@params[['region']]}
 
-## Set variables  ---------------------------------------------------------------------
-data_dir <- '~/Desktop/dystonia_snRNAseq_2024/resources/'
-script_dir <- '~/Desktop/dystonia_snRNAseq_2024/workflow/scripts/'
-results_dir <- '~/Desktop/dystonia_snRNAseq_2024/results/'
-stiletti_dir <- paste0(data_dir, 'public_data/stiletti_2023/')
-R_dir <- paste0(results_dir, '01R/')
-markdown_doc <- paste0(script_dir, 'dystonia_qc.Rmd')
-
-regions <- c('fcx', 'str', 'glp', 'cer')
-fcx_anns <- c('A13', 'A14', 'A25', 'A32', 'A44-A45', 'A46', 'FI', 'M1C')
-str_anns <- c('CaB', 'Pu')
-cer_anns <- c('CBL', 'CBV', 'CbDN')
-#glp_anns <- c('GPi', 'GPe')
-all_anns <- c(fcx_anns, str_anns, cer_anns)
-anns_table <- read_excel(paste0(data_dir, 'sheets/Stiletti_downloads_table.xlsx'))
-dystonia_genes <- read_excel(paste0(data_dir, 'sheets/Dystonia_Genes_Clinical_5.0.xlsx'), range = 'D1:D26') %>%
-  pull(GeneName) 
-  
-options(future.globals.maxSize = 3e+09) # set this option when analyzing large datasets
-options(digits = 1) # Set default decimal points
-options(scipen = 999)
-
-region <- 'str'
-markdown_html <- paste0('dystonia_qc_', toupper(region), '.html')
-
-# Set region specific variables
-
-if (region == 'fcx') {
-  
-  pc_thresh <- 50
-  sample_split <- 'orig.ident'
-  
-} else if (region == 'cer') {
-  
-  pc_thresh <- 30
-  sample_split <- 'orig.ident'
-  
-} else {
-  
-  pc_thresh <- 30
-  sample_split <- 'sample_id'
-  
-}
-
-## Load functions, gene lists and colours  --------------------------------------------
+##  Load Packages, functions and variables  -------------------------------------------
+source('~/Desktop/dystonia_snRNAseq_2024/workflow/scripts/Renvs.R')
 source('~/Desktop/dystonia_snRNAseq_2024/workflow/scripts/dystonia_functions.R')
 source('~/Desktop/dystonia_snRNAseq_2024/workflow/scripts/dystonia_gene_lists.R')
   
