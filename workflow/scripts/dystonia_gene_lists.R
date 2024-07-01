ce60a8d2d3a62320b8bd8041abcf1cf310deeaa9
Dazcam <camerond@cardiff.ac.uk>
2024-07-01 09:47:16 +0100

    Add fetal gene lists / Fix FC miscoding

--- workflow/scripts/dystonia_gene_lists.R
+++ workflow/scripts/dystonia_gene_lists.R
@@ -34,6 +34,15 @@ str_genes <- c('DRD1', 'DRD2', 'TAC1', 'PENK',
                'CCK', 'TH', 'CHAT', 'CALB1', 'SOX4', 'DLX2',
                'LHX6', 'LHX8', 'NKX2.1', 'NR2F2')
 
+# See: [Saunders et al. 2018](https://doi.org/10.1016/j.cell.2018.07.028)
+small_populations <- c('VTN', 'KCNJ8', 'ABCC9', 'ART3',  # Pericytes
+                       'ACTA2', 'RGS5', 'ALDH1A1',      # Smooth muscle cells
+                       'SLC38A2', 'SLC4A10', 'SLC26A2', 'SLC47A1', 'FXYD5',  # Fibroblast
+                       'ATP1B1', 'COL4A1', 'COL4A2', 'COL15A1', 'COLA1',
+                       'COL3A1',
+                       'TM4SF1', 'SLC38A5', 'CYTL1', 'BMX', 'MGP',    # Endothelial
+                       'FBLN5', 'ELN', 'IGFBP4', 'CLU')
+
 str_final_genes <- factor(c('GAD1', 'GAD2', 'SLC6A1', 'DRD1', 'DRD2', 
                             'SST', 'NPY', 'CALB2', 'VIP', 'NPY', 'CHAT', 
                             'SLC17A7','GFAP', 'AQP4', 'SOX9', 'OLIG1', 
@@ -70,14 +79,14 @@ cer_final_genes <- factor(c('GAD1', 'GAD2', 'PVALB', 'SLC17A7',
                             'C3', 'C1QB', 'EBF1', 'ABCB1', 'APOD'))
 
 
-cer_umap_cols_recode <- c("Cer-adult-InN-1" = '#95D840FF', "Cer-adult-InN-2" = '#31C53F', "Cer-adult-InN-3" = '#3CBB75FF', 
-                          "Cer-adult-ExN" = '#00B6EB', "Cer-adult-UBC" = '#CEE5FD', "Cer-adult-BGli?" = '#6F2DA8', 
+cer_umap_cols_recode <- c("Cer-adult-ExN" = '#00B6EB', "Cer-adult-UBC" = '#CEE5FD', "Cer-adult-InN-1" = '#95D840FF', 
+                          "Cer-adult-InN-2" = '#31C53F', "Cer-adult-InN-3" = '#3CBB75FF', "Cer-adult-BGli?" = '#6F2DA8', 
                           "Cer-adult-Olig" = '#FDE725FF', "Cer-adult-OPC" = '#FFBF00', "Cer-adult-Ast" = '#FF5959', 
                           "Cer-adult-MG" = '#F58231', "Cer-adult-Endo?" = '#9A6324', "Cer-adult-Pericyte?" = '#CCCCCC',
                           "Cer-adult-Leuko?" = "#F032E6")
 
-cer_vln_cols_recode <- c("Cer-adult-InN-1" = '#3CBB75FF', "Cer-adult-InN-2" = '#3CBB75FF', "Cer-adult-InN-3" = '#3CBB75FF', 
-                         "Cer-adult-ExN" = '#00B6EB', "Cer-adult-UBC" = '#00B6EB', "Cer-adult-BGli?" = '#6F2DA8', 
+cer_vln_cols_recode <- c("Cer-adult-ExN" = '#00B6EB', "Cer-adult-UBC" = '#00B6EB', "Cer-adult-InN-1" = '#3CBB75FF', 
+                         "Cer-adult-InN-2" = '#3CBB75FF', "Cer-adult-InN-3" = '#3CBB75FF', "Cer-adult-BGli?" = '#6F2DA8', 
                          "Cer-adult-Olig" = '#FDE725FF', "Cer-adult-OPC" = '#FFBF00', "Cer-adult-Ast" = '#FF5959', 
                          "Cer-adult-MG" = '#F58231', "Cer-adult-Endo?" = '#9A6324', "Cer-adult-Pericyte?" = '#CCCCCC',
                          "Cer-adult-Leuko?" = "#F032E6")
@@ -109,23 +118,44 @@ fcx_final_genes <- factor(c('SLC17A7', 'RORB', 'TLE4', 'GAD1', 'GAD2',
                            'OLIG2', 'MBP', 'GFAP', 'SOX9', 'AQP4', 
                             'C3', 'C1QB', 'EBF1', 'ABCB1', 'APOD'))
 
-fcx_umap_cols_recode <- c("Ctx-adult-ExN-1" = '#00B6EB', "Ctx-adult-ExN-2" = '#CEE5FD', "Ctx-adult-ExN-3" = '#598BAF',
-                          "Ctx-adult-ExN-4" = '#6693F5', "Ctx-adult-ExN-5" = '#89CFEF', "Ctx-adult-ExN-6" = '#95C8D8', 
-                          "Ctx-adult-ExN-7" = '#4682B4', "Ctx-adult-ExN-8" = '#73C2FB', "Ctx-adult-InN-1" = '#708238', 
-                          "Ctx-adult-InN-2" = '#31C53F', "Ctx-adult-InN-3" = '#3CBB75FF', "Ctx-adult-InN-4" = '#9DC183', 
-                          "Ctx-adult-InN-5" = '#006400', "Ctx-adult-InN-6"= '#95D840FF', "Ctx-adult-InN-7" = '#2FF18B', 
-                          "Ctx-adult-InN-8" = '#B7FFB7', "Ctx-adult-Olig" = '#FDE725FF', "Ctx-adult-OPC" = '#FFBF00', 
-                          "Ctx-adult-Ast" = '#FF5959', "Ctx-adult-MG" = '#F58231', "Ctx-adult-Undef" = '#CCCCCC')
+fcx_umap_cols_recode <- c("FC-adult-ExN-1" = '#00B6EB', "FC-adult-ExN-2" = '#CEE5FD', "FC-adult-ExN-3" = '#598BAF',
+                          "FC-adult-ExN-4" = '#6693F5', "FC-adult-ExN-5" = '#89CFEF', "FC-adult-ExN-6" = '#95C8D8', 
+                          "FC-adult-ExN-7" = '#4682B4', "FC-adult-ExN-8" = '#73C2FB', "FC-adult-InN-1" = '#708238', 
+                          "FC-adult-InN-2" = '#31C53F', "FC-adult-InN-3" = '#3CBB75FF', "FC-adult-InN-4" = '#9DC183', 
+                          "FC-adult-InN-5" = '#006400', "FC-adult-InN-6"= '#95D840FF', "FC-adult-InN-7" = '#2FF18B', 
+                          "FC-adult-InN-8" = '#B7FFB7', "FC-adult-Olig" = '#FDE725FF', "FC-adult-OPC" = '#FFBF00', 
+                          "FC-adult-Ast" = '#FF5959', "FC-adult-MG" = '#F58231', "FC-adult-Undef" = '#CCCCCC')
 
 
-fcx_vln_cols_recode <- c("Ctx-adult-ExN-1" = '#00B6EB', "Ctx-adult-ExN-2" = '#00B6EB', "Ctx-adult-ExN-3" = '#00B6EB',
-                         "Ctx-adult-ExN-4" = '#00B6EB', "Ctx-adult-ExN-5" = '#00B6EB', "Ctx-adult-ExN-6" = '#00B6EB', 
-                         "Ctx-adult-ExN-7" = '#00B6EB', "Ctx-adult-ExN-8" = '#00B6EB', "Ctx-adult-InN-1" = '#3CBB75FF', 
-                         "Ctx-adult-InN-2" = '#3CBB75FF', "Ctx-adult-InN-3" = '#3CBB75FF', "Ctx-adult-InN-4" = '#3CBB75FF', 
-                         "Ctx-adult-InN-5" = '#3CBB75FF', "Ctx-adult-InN-6"= '#3CBB75FF', "Ctx-adult-InN-7" = '#3CBB75FF', 
-                         "Ctx-adult-InN-8" = '#3CBB75FF', "Ctx-adult-Olig" = '#FDE725FF', "Ctx-adult-OPC" = '#FFBF00', 
-                         "Ctx-adult-Ast" = '#FF5959', "Ctx-adult-MG" = '#F58231', "Ctx-adult-Undef" = '#CCCCCC')
+fcx_vln_cols_recode <- c("FC-adult-ExN-1" = '#00B6EB', "FC-adult-ExN-2" = '#00B6EB', "FC-adult-ExN-3" = '#00B6EB',
+                         "FC-adult-ExN-4" = '#00B6EB', "FC-adult-ExN-5" = '#00B6EB', "FC-adult-ExN-6" = '#00B6EB', 
+                         "FC-adult-ExN-7" = '#00B6EB', "FC-adult-ExN-8" = '#00B6EB', "FC-adult-InN-1" = '#3CBB75FF', 
+                         "FC-adult-InN-2" = '#3CBB75FF', "FC-adult-InN-3" = '#3CBB75FF', "FC-adult-InN-4" = '#3CBB75FF', 
+                         "FC-adult-InN-5" = '#3CBB75FF', "FC-adult-InN-6"= '#3CBB75FF', "FC-adult-InN-7" = '#3CBB75FF', 
+                         "FC-adult-InN-8" = '#3CBB75FF', "FC-adult-Olig" = '#FDE725FF', "FC-adult-OPC" = '#FFBF00', 
+                         "FC-adult-Ast" = '#FF5959', "FC-adult-MG" = '#F58231', "FC-adult-Undef" = '#CCCCCC')
   
+# Fetal gene sets
+fetal_fcx_vln_recode <- c("FC-fetal-ExN-1" = '#00B6EB', "FC-fetal-ExN-2" = '#00B6EB', "FC-fetal-ExN-3" = '#00B6EB', 
+                          "FC-fetal-ExN-4" = '#00B6EB', "FC-fetal-ExN-5" = '#00B6EB', "FC-fetal-InN-1" = '#3CBB75FF', 
+                          "FC-fetal-InN-2" = '#3CBB75FF', "FC-fetal-InN-3" = '#3CBB75FF', "FC-fetal-InN-4" = '#3CBB75FF', 
+                          "FC-fetal-OPC" = '#FDE725FF', "FC-fetal-RG-1" = '#FF5959', "FC-fetal-RG-2" = '#FF5959', 
+                          "FC-fetal-MG" = '#F58231', "FC-fetal-CycPro" = '#DCBEFF', "FC-fetal-IP" = '#B200ED', 
+                          "FC-fetal-Endo" = '#9A6324', "FC-fetal-N-undef" = '#CCCCCC')
+
+fetal_cer_vln_recode <- c("Cer-fetal-ExN-1" = '#00B6EB', "Cer-fetal-ExN-2" = '#00B6EB', "Cer-fetal-ExN-3" = '#00B6EB', 
+                          "Cer-fetal-ExN-4" = '#00B6EB', "Cer-fetal-ExN-5" = '#00B6EB', "Cer-fetal-InN-1" = '#3CBB75FF', 
+                          "Cer-fetal-InN-2" = '#3CBB75FF', "Cer-fetal-InN-3" = '#3CBB75FF', "Cer-fetal-InN-4" = '#3CBB75FF', 
+                          "Cer-fetal-InN-5" = '#3CBB75FF', "Cer-fetal-InN-6" = '#3CBB75FF', "Cer-fetal-InN-7" = '#3CBB75FF',
+                          "Cer-fetal-InN-8" = '#3CBB75FF', "Cer-fetal-OPC" = '#FDE725FF', "Cer-fetal-RG-1" = '#FF5959', 
+                          "Cer-fetal-RG-2" = '#FF5959', "Cer-fetal-RG-3" = '#FF5959', "Cer-fetal-MG" = '#F58231',
+                          "Cer-fetal-CycPro" = '#DCBEFF',  "Cer-fetal-IP" = '#B200ED', "Cer-fetal-Endo" = '#9A6324')
+
+fetal_ge_vln_recode <- c("GE-fetal-InN-1" = '#3CBB75FF', "GE-fetal-InN-2" = '#3CBB75FF', "GE-fetal-InN-3" = '#3CBB75FF',
+                         "GE-fetal-InN-4" = '#3CBB75FF', "GE-fetal-InN-5" = '#3CBB75FF', "GE-fetal-InN-6" = '#3CBB75FF', 
+                         "GE-fetal-InN-7" = '#3CBB75FF', "GE-fetal-RG-1" = '#FF5959', "GE-fetal-RG-2" = '#FF5959', 
+                         "GE-fetal-RG-3" = '#FF5959', "GE-fetal-CycPro" = '#DCBEFF')
+
 # Colours
 greens <- c('#3CBB75FF', '#00FF00A5','#006400', '#B7FFB7', '#10A53DFF',
             '#95D840FF', '#9DC183',  '#708238', '#55C667FF', '#73D055FF',
@@ -136,7 +166,7 @@ ExN_blues <- c('#76B5C5', '#00BDD2', '#CEE5FD', '#00B6EB', '#ABDBE3',
 
 reds <- c('#FAA0A0', '#FF5959', '#F75151', '#EF0029', '#D2042D')
 
-reds <- c('#B200ED',  '#DCBEFF', '#6F2DA8')
+purples <- c('#B200ED',  '#DCBEFF', '#6F2DA8')
 
 #--------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------
