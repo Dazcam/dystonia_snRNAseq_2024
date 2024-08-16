1a77c67a7eab4ab461bf1aecac6e8654cb2995e5
Dazcam <camerond@cardiff.ac.uk>
2024-08-16 09:41:50 +0100

    MatrixExtra clashing with Seurat. Comment out for now.

--- workflow/scripts/dystonia_Renvs.R
+++ workflow/scripts/dystonia_Renvs.R
@@ -23,7 +23,8 @@ library(cowplot)
 library(scuttle)
 library(scater)
 library(egg) 
-library(MatrixExtra)
+#library(MatrixExtra) # This clashes with Seurat. Used here for hdWGCNA markdown
+# Need to load within script just before markdown. See https://github.com/satijalab/seurat/issues/9221
 
 library(tidyverse)
 library(cowplot)
