4ef5e9e584002f34449aff9259ae51eec2000758
Dazcam <camerond@cardiff.ac.uk>
2024-10-31 13:26:23 +0000

    Code tweaks for Hawk run v7

--- workflow/scripts/dystonia_calc_pseudobulk_and_aver_expression.R
+++ workflow/scripts/dystonia_calc_pseudobulk_and_aver_expression.R
@@ -27,23 +27,24 @@ if (Sys.info()[["nodename"]] == "Darrens-iMac-2.local") {
 
 ## Load Data --------------------------------------------------------------------------
 seurat_object <- readRDS(paste0(R_dir, '03seurat_', region, '.rds'))
-fetal_region <- str_split_i(fetal_region, '_', 1)
-seurat_fetal <- readRDS(paste0(fetal_dir, 'seurat_', fetal_region, '_fetal.rds'))
-Idents(seurat_fetal) <- 'cellIDs'
+# fetal_region <- str_split_i(fetal_region, '_', 1)
+# seurat_fetal <- readRDS(paste0(fetal_dir, 'seurat_', fetal_region, '_fetal.rds'))
+# Idents(seurat_fetal) <- 'cellIDs'
 
 # Switch to whole dataset
 message('\nChanging to RNA object ...\n')
 DefaultAssay(seurat_object) <- 'RNA'
 seurat_object <- JoinLayers(seurat_object) # Do this for sketch and RNA independently
+message('Number of NAs in Idents: ', anyNA(Idents(seurat_object)))
 
-# Recode cluster IDs - sketch object 
-seurat_object[[paste0(region, '_clusters')]] <- recode_cluster_ids(seurat_object, 
-                                                                   region, 
-                                                                   'cluster_full')
-
-# Set Idents 
-message("Any NAs after recode of cluster IDs ?",  seurat_object[[paste0(region, '_clusters')]] |> anyNA())
-#Idents(seurat_object) <- seurat_object[[paste0(region, '_clusters')]]
+# # Recode cluster IDs - sketch object 
+# seurat_object[[paste0(region, '_clusters')]] <- recode_cluster_ids(seurat_object, 
+#                                                                    region, 
+#                                                                    'cluster_full')
+# 
+# # Set Idents 
+# message("Any NAs after recode of cluster IDs ?",  seurat_object[[paste0(region, '_clusters')]] |> anyNA())
+# #Idents(seurat_object) <- seurat_object[[paste0(region, '_clusters')]]
 
 # Calculate aggregate and average expression - adult
 ag_adult_mat <- calculate_aggregated_expression(seurat_object, dystonia_genes)
