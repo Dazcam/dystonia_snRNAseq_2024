4db7dd57c3ce2fd3c368dae7ae72d939591fa4de
Dazcam <camerond@cardiff.ac.uk>
2025-10-09 16:55:59 +0100

    Add sng to env file

--- workflow/scripts/dystonia_Renvs.R
+++ workflow/scripts/dystonia_Renvs.R
@@ -87,11 +87,12 @@ markdown_ann_doc <- paste0(script_dir, 'dystonia_ann.Rmd')
 markdown_ann_html <- paste0('dystonia_ann_', region, '.html')
 markdown_wgcna_doc <- paste0(script_dir, 'dystonia_wgcna.Rmd')
 markdown_wgcna_html <- paste0('dystonia_wgcna_', region, '.html')
-regions <- c('fcx', 'str', 'cer')
+regions <- c('fcx', 'str', 'cer', 'sng')
 fcx_anns <- c('A13', 'A14', 'A25', 'A32', 'A44-A45', 'A46', 'FI', 'M1C')
 str_anns <- c('CaB', 'Pu')
 cer_anns <- c('CBL', 'CBV', 'CbDN')
-all_anns <- c(fcx_anns, str_anns, cer_anns)
+sng_anns <- c('SN')
+all_anns <- c(fcx_anns, str_anns, cer_anns, sng_anns)
 anns_table <- read_excel(paste0(data_dir, 'sheets/Stiletti_downloads_table.xlsx'))
 sample_split <- 'sample_id' # Meta_id col to split seurat object by for qc
 resolution_set <- seq(0.1, 0.8, 0.1) # Set of res params to test
@@ -113,10 +114,12 @@ message(length(dystonia_genes), ' genes in dystonia hgnc_hg38 gene list.')
 
 # Set num of cells per sample to create sketch object of ~60K
 # Note FCX sample 10X418_6 only has 366 cells
+# Note SNG added in review, fewer cells / samples but used sketch for consistency
 sketch_num <- dplyr::case_when(
   region == "fcx" ~ 1100, # 56 samples (56 * 1100 = 61600 cells)
   region == "str" ~ 5000, # 12 samples (12 * 5000 = 60000 cells)
-  region == "cer" ~ 2100) # 28 samples (28 * 2200 = 60200 cells)
+  region == "cer" ~ 2100, # 28 samples (28 * 2200 = 60200 cells)
+  region == "sng" ~ 4150) # 10 samples (10 * 4150 = 41500 cells) # Added in review
 
 ### For final violin plots
 
